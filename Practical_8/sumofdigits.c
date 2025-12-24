49592adab23c4cc59ed4788cc91be85d79547747
bhaskar ranjan <ombhaskar2252005@gmail.com>
2025-12-24 23:19:11 +0530

    add files

---
+++ Practical_8/sumofdigits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int sum_of_digits(int n) {
+    int sum = 0;
+    while (n != 0) {
+        sum += n % 10; 
+        n /= 10;       
+    }
+    return sum;
+}
+int main() {
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    int result = sum_of_digits(number);
+    printf("Sum of digits: %d\n", result);
+    return 0;
+}
